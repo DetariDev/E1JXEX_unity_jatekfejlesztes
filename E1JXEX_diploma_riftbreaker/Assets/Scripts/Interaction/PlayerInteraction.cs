f470d8974c24322cd7e1d3e6a09a1f6f3988f350
Détári Levi <detarilevi@gmail.com>
2025-11-27 14:40:47 +0100

    Interakció kezelés hozzáadva
    Introduced new interaction system with IInteractable, PlayerInteraction, and WeaponPickup scripts. Refactored project structure by moving InputManager, PlayerManager, WeaponManager, PlayerAim, PlayerAttack, and PlayerMovement into Managers and Player folders. Added TextMesh Pro examples and assets for UI enhancements.

---
+++ E1JXEX_diploma_riftbreaker/Assets/Scripts/Interaction/PlayerInteraction.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    [SerializeField] private PlayerAim playerAim;
+    [SerializeField] private float maxDistance = 6f;
+    [SerializeField] private LayerMask interactionLayer;
+
+    private InputSystemActions.PlayerActions playerInput;
+    private Camera cam;
+
+    private void Start()
+    {
+        if (!playerAim) playerAim = GetComponent<PlayerAim>();
+        playerInput = InputManager.instance.input.Player;
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (playerInput.Interact.WasPerformedThisFrame())
+        {
+            TryInteract();
+        }
+    }
+
+    private void TryInteract()
+    {
+        if (!playerAim.aimTarget) return;
+
+        Vector3 rayDirection = playerAim.aimTarget.position - cam.transform.position;
+
+        if (Physics.Raycast(cam.transform.position, rayDirection, out RaycastHit hit, Mathf.Infinity, interactionLayer))
+        {
+            if ((transform.position - hit.transform.position).sqrMagnitude <= maxDistance * maxDistance)
+            {
+                if (hit.collider.TryGetComponent(out IInteractable interactable))
+                {
+                    interactable.Interact();
+                }
+            }
+        }
+    }
+}
---
+++ E1JXEX_diploma_riftbreaker/Assets/Scripts/Interaction/WeaponPickup.cs
@@ -0,0 +1,19 @@
+using TMPro;
+using UnityEngine;
+
+public class WeaponPickup : MonoBehaviour, IInteractable
+{
+    public TMP_Text title;
+    public Weapon weapon;
+    void Start()
+    {
+        title.text = weapon.weaponName;
+    }
+    public void Interact()
+    {
+        Weapon temp = WeaponManager.instance.currentWeapon;
+        WeaponManager.instance.currentWeapon = weapon;
+        weapon = temp;
+        title.text = weapon.weaponName;
+    }
+}
