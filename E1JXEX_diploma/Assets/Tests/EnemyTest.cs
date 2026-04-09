e3350b318e9d84faf6bb26ac9669c8cde9c51bd9
Détári Levi <detarilevi@gmail.com>
2026-04-09 12:55:48 +0200

    profiler

--- E1JXEX_diploma/Assets/Tests/EnemyTest.cs
+++ E1JXEX_diploma/Assets/Tests/EnemyTest.cs
@@ -7,28 +7,5 @@ using UnityEngine.TestTools;
 
 public class EnemyCombatIntegrationTests
 {
-    private GameObject enemy;
-    private GameObject wall;
-    private GameObject turret;
-
-    [SetUp]
-    public void Setup()
-    {
-        
-    }
-
-    [TearDown]
-    public void Teardown()
-    {
-        // A teszt végén eltakarítunk magunk után, hogy a következõ teszt tiszta lappal induljon
-        if (enemy != null) Object.DestroyImmediate(enemy);
-        if (wall != null) Object.DestroyImmediate(wall);
-        if (turret != null) Object.DestroyImmediate(turret);
-    }
-
-    [UnityTest]
-    public IEnumerator EnemyAttacksWall_ThenTurretKillsEnemy()
-    {
-        
-    }
+    
 }
